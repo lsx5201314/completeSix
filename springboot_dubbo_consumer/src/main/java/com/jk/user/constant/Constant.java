db4e759104a3430512024092a44f5c445ca35b87
ygq <704292061@qq.com>
2019-11-18 09:12:36 +0800

    redis缓存登录用户10天

---
+++ springboot_dubbo_consumer/src/main/java/com/jk/user/constant/Constant.java
@@ -0,0 +1,13 @@
+package com.jk.user.constant;
+
+/**
+ * @ClassName Constant
+ * @Deacription TODO
+ * @Author 原国庆
+ * @Date 2019/11/18 8:46
+ * @Version 1.0
+ **/
+public class Constant {
+    public static  final  String  KEY="cmsUser";
+
+}
